0e90bfe639ebaa8db5db5dd86011ef05304d218d
Ivan Neretin <neretinivan17@yandex.ru>
2022-02-14 01:15:47 +0300

    Damage mechanics added

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const scorpionImage = './assets/scorpion.gif';
 const sonyaImage = './assets/sonya.gif';
 
+
 class Player {
-    constructor(name, baseHp, img, weapons){
+    static playerIDs = {playerOne: 1, playerTwo: 2};
+
+    constructor(name, img, weapons, id){
         this.name = name;
-        this.baseHp = baseHp;
+        this.currentHp = 100;
         this.img = img;
         this.weapons = weapons;
+        this.id = id;
     }
 
-    Attack () {
-        console.log(this.name + " is attacking!");
+    Attack (enemyPlayer) {
+        console.log(this.name + " is attacking! ");
+        enemyPlayer.ReceiveDamage(this.#GetDamage(), this);
     }
     
+    #GetDamage () {
+        return Math.ceil(Math.random() * 10);
+    }
+
+    ReceiveDamage (damage, source) {
+        if (damage > this.currentHp) damage = this.currentHp;
+        this.currentHp -= damage;
+        this.$lifeBar.style.width = this.currentHp + '%';
+        console.log(this.name + " got " + damage + " damage from " + source.name + ". " + this.currentHp + " hp left");
+        if (this.currentHp <= 0) this.#BecomeDead(source);
+    }
+
+    #BecomeDead (source) {
+        GameIsOver(source);
+    }
+
     CreatePlayer (playerId) {
         const $player = document.createElement('div');
         $player.classList.add(playerId);
@@ -33,6 +54,7 @@ class Player {
         const $life = document.createElement('div');
         $life.classList.add('life');
         $life.style.width = '100%';
+        this.$lifeBar = $life;
         return $life;
     }
 
@@ -59,12 +81,36 @@ class Player {
 
 /* ******** */
 
-const player1 = new Player('Scorpion', 100, scorpionImage, ['knife', 'chain']);
-const player2 = new Player('Sonya', 120, sonyaImage, ['axe', 'sword']);
-
-player1.Attack();
-player2.Attack();
+const player1 = new Player('Scorpion', scorpionImage, ['knife', 'chain'], Player.playerIDs.playerOne);
+const player2 = new Player('Sonya', sonyaImage, ['axe', 'sword'], Player.playerIDs.playerTwo);
 
 const $arena = document.getElementsByClassName('arenas')[0];
 $arena.append(player1.CreatePlayer('player1'));
 $arena.append(player2.CreatePlayer('player2'));
+
+const $randomButton = document.getElementsByClassName('button')[0];
+$randomButton.addEventListener('click', onRandomButtonClick);
+
+function onRandomButtonClick () {
+    if (Math.random() > 0.5) {
+        player1.Attack(player2);
+    }
+    else {
+        player2.Attack(player1);
+    }
+}
+
+function GameIsOver (winner) {
+    const winMessage = winner.name + " wins!";
+    console.log(winMessage);
+    const $randomButton = document.getElementsByClassName('button')[0];
+    $randomButton.disabled = true;
+    $arena.append(CreateMessage(winMessage));
+}
+
+function CreateMessage (message) {
+    const $messageLabel = document.createElement('div');
+    $messageLabel.innerText = message;
+    $messageLabel.classList.add('loseTitle');
+    return $messageLabel;
+}
